76a351ac855d162f90cb14f862c62b996a2b2ff8
Sean Morgan <scm0323@att.net>
2021-08-19 15:12:22 -0500

    sololearn exercise one

---
+++ Variables1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Variables1
+{
+    class Program
+        {
+            static void Main(string[] args)
+            {
+                string name = "Chris Bradley";
+                int year = 2021;
+                char letter = 'Y';
+                bool isFunny = true;
+                double rate = 7.5;
+                decimal pay = 75.00m;
+
+                Console.WriteLine($"{name}, who joined TrueCoders in {year}; reasons {letter}, who knows. " +
+                    $"{isFunny}, he is funny; though his humor is on a rate of {rate} on the rediculous scale. " +
+                    $"Though, he does pay each of us {pay} par hour to laugh at his jokes.");
+            }
+    }
+}
